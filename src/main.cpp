792d157a7d04c34c2a51462c2409d909c11e59ab
kevin <kevingarcia0808@icloud.com>
2025-08-26 17:47:37 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -225,6 +225,7 @@ void opcontrol() {
     // . . .
     // Put more user control code here!
     // . . .
+    //test
 
     if (master.get_digital(DIGITAL_L1)){   // in-take through 
       frontIn.move(63.75);
